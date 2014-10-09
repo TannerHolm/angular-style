65a77d7cb81b96d0443122fa7490deb1d796e163
TannerHolm <holm.tanner@gmail.com>
2014-10-09 10:44:08 -0600

    views in place, just add content

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = angular.module("app", ["ngRoute", "ngAnimate"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/',
+      {
+        templateUrl: "views/welcome.html",
+        controller: "AppCtrl"
+      }
+    )
+    .when('/portfolio',
+      {
+        templateUrl: "views/portfolio.html",
+        controller: "AppCtrl"
+      }
+    )
+
+});
+
+app.controller("AppCtrl", function($scope) {
+    $scope.model = {
+        message: "This is my app!!!"
+    }
+})
